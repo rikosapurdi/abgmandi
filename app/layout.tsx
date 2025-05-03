7c937068c5c1591dca0ad96a88a57f239a9cf647
rikosapurdi <rikosapurdi@gmail.com>
2025-05-04 05:18:04 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     },
     verification: {
         google: 'umSh7RIP5KkXhPH-HOZBCGsIb99xBxEJ8E_0vieP5oQ',
-        yandex: '628735a3e910495b',
+        yandex: '8e9056ea2ddde803',
     },
 };
 
@@ -46,17 +46,6 @@ export default function RootLayout({
             },
             "foundingDate":"2024-09-16"
         }
-        const jsonLd1 = {
-            "@context": "https://schema.org",
-        "@type": "WebPage",
-        "name": `${SITENAME}`,
-        "url": "https://abgmandi.pages.dev",
-        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
-        "image": "https://abgmandi.pages.dev/favicon.ico",
-        "potentialAction":{
-            "@type":"ReadAction",
-            "target":"https://abgmandi.pages.dev"}
-        }
         const jsonLd2 = {
             "@context": "https://schema.org",
         "@type": "WebSite",
@@ -80,10 +69,6 @@ export default function RootLayout({
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
         />
         <script
           type="application/ld+json"
